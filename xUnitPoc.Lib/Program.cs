e93a2221f1680f21074cdff164b4055d45c8d037
gemah <hianneiva@gmail.com>
2019-10-17 10:20:32 -0300

    PoC Lib portion upload

---
+++ xUnitPoc.Lib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace xUnitPoc.Lib
+{
+    public class Program
+    {
+        [ExcludeFromCodeCoverage]
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\nPlease run the tests according to what is available.");
+        }
+    }
+}
---
+++ xUnitPoc.Lib/SimpleMath.cs
@@ -0,0 +1,32 @@
+﻿namespace xUnitPoc.Lib
+{
+    public class SimpleMath
+    {
+        public static int AddInt(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int SubInt(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int MulInt(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int DivInt(int a, int b)
+        {
+            if (b == 0)
+            {
+                return int.MaxValue;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
